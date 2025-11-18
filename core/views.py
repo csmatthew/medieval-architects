83ce6936a2bfe6151d4b45b4aa8f57ad2453fd9d
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2025-11-18 23:06:28 +0000

    Feat: simple search feature

--- core/views.py
+++ core/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from .models import Craftsperson, Building
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "core/index.html")
+
+
+def search(request):
+    q = request.GET.get('q', '').strip()
+    craftspersons = Craftsperson.objects.none()
+    buildings = Building.objects.none()
+    if q:
+        craftspersons = Craftsperson.objects.filter(
+            Q(name__icontains=q) |
+            Q(forename__icontains=q) |
+            Q(label__icontains=q) |
+            Q(sequence_label__icontains=q)
+        ).order_by('name')[:100]
+        buildings = Building.objects.filter(
+            Q(name__icontains=q)
+        ).order_by('name')[:100]
+    context = {
+        'query': q,
+        'craftspersons': craftspersons,
+        'buildings': buildings,
+    }
+    return render(request, "core/search_results.html", context)
