31c0dcf5d56e27714c1d7ef58dded39be01f6ea1
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2026-05-14 23:04:21 +0100

    add models people

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        "full_name", "surname", "given_name", "label", "sequence_label"
+    )
+    search_fields = ("surname", "given_name", "label")
+    ordering = ("surname", "given_name", "sequence_label")
--- people/models.py
+++ people/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    surname = models.CharField(max_length=100, blank=True, null=True)
+    given_name = models.CharField(max_length=100, blank=True, null=True)
+    preposition = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        help_text="e.g. de, von, van, ap, fitz"
+    )
+    label = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text="e.g. the Mason, the Elder, the Younger"
+    )
+    sequence_label = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        help_text="e.g. II, III, Junior"
+    )
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        """
+        Builds a readable name like:
+        'Nicholas de Ailington the Mason II'
+        """
+        parts = []
+
+        if self.given_name:
+            parts.append(self.given_name)
+
+        if self.preposition:
+            parts.append(self.preposition)
+
+        if self.surname:
+            parts.append(self.surname)
+
+        if self.label:
+            parts.append(self.label)
+
+        if self.sequence_label:
+            parts.append(self.sequence_label)
+
+        return " ".join(parts)
