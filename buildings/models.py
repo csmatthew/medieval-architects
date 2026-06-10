284691978eb36a88563e11f040a1c9749fa3c4e0
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2026-06-10 02:24:42 +0100

    add created_at field

--- buildings/models.py
+++ buildings/models.py
@@ -13,5 +13,7 @@ class Building(models.Model):
         help_text="People associated with this building"
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
