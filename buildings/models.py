8c2a1fd12c68fe27fd79d6e940152e0a49a9839c
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2026-06-10 23:47:24 +0100

    add historic counties to building list

--- buildings/models.py
+++ buildings/models.py
@@ -13,6 +13,57 @@ class Building(models.Model):
         help_text="People associated with this building"
     )
 
+    county_choices = [
+            ("Bedfordshire", "Bedfordshire"),
+            ("Berkshire", "Berkshire"),
+            ("Buckinghamshire", "Buckinghamshire"),
+            ("Cambridgeshire", "Cambridgeshire"),
+            ("Cheshire", "Cheshire"),
+            ("Cornwall", "Cornwall"),
+            ("Cumberland", "Cumberland"),
+            ("Derbyshire", "Derbyshire"),
+            ("Devon", "Devon"),
+            ("Dorset", "Dorset"),
+            ("Durham", "Durham"),
+            ("Essex", "Essex"),
+            ("Gloucestershire", "Gloucestershire"),
+            ("Hampshire", "Hampshire"),
+            ("Herefordshire", "Herefordshire"),
+            ("Hertfordshire", "Hertfordshire"),
+            ("Huntingdonshire", "Huntingdonshire"),
+            ("Kent", "Kent"),
+            ("Lancashire", "Lancashire"),
+            ("Leicestershire", "Leicestershire"),
+            ("Lincolnshire", "Lincolnshire"),
+            ("Middlesex", "Middlesex"),
+            ("Norfolk", "Norfolk"),
+            ("Northamptonshire", "Northamptonshire"),
+            ("Northumberland", "Northumberland"),
+            ("Nottinghamshire", "Nottinghamshire"),
+            ("Oxfordshire", "Oxfordshire"),
+            ("Rutland", "Rutland"),
+            ("Shropshire", "Shropshire"),
+            ("Somerset", "Somerset"),
+            ("Staffordshire", "Staffordshire"),
+            ("Suffolk", "Suffolk"),
+            ("Surrey", "Surrey"),
+            ("Sussex", "Sussex"),
+            ("Warwickshire", "Warwickshire"),
+            ("Westmorland", "Westmorland"),
+            ("Wiltshire", "Wiltshire"),
+            ("Worcestershire", "Worcestershire"),
+            ("Yorkshire", "Yorkshire"),
+        ]
+    """https://en.wikipedia.org/wiki/Historic_counties_of_England"""
+
+    county = models.CharField(
+        max_length=20,
+        choices=county_choices,
+        blank=True,
+        null=True,
+        help_text="Historic county in England"
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
