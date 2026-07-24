7d35364f6d319f1b2e8d4c9f09cf2f361e7803e7
Christopher S. Matthew <christopher.s.matthew@gmail.com>
2026-07-24 15:45:45 +0100

    HTML refine, seperate concerns for nav/foot

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import about_view
+from django.urls import include, path
+from .views import about_view, search_view
 
 urlpatterns = [
     path('about/', about_view, name='about'),
+    path('search/', search_view, name='search'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,10 @@ def about_view(request):
     View for the about page.
     """
     return render(request, 'core/about.html')
+
+
+def search_view(request):
+    """
+    View for the search page.
+    """
+    return render(request, 'core/search.html')
